41f050217431a0f7d641dac0b00f3b41a24e7e96
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2013-10-03 21:50:47 +0200

    Version bump to 1.2, use proxy streaming to make it work better.

--- Allelon/src/main/java/com/ciplogic/allelon/AllelonMediaPlayer.java
+++ Allelon/src/main/java/com/ciplogic/allelon/AllelonMediaPlayer.java
@@ -2,22 +2,39 @@ package com.ciplogic.allelon;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 
 // views have their own media player, thus this object is static.
 public class AllelonMediaPlayer {
     private static MediaPlayer mediaPlayer;
     private static boolean playing;
 
+    private StreamProxy proxy;
+
     public boolean isPlaying() {
         return playing;
     }
 
     public void startPlay(String url) {
+        int sdkVersion = 0;
+        try {
+            sdkVersion = Integer.parseInt(Build.VERSION.SDK);
+        } catch (NumberFormatException ignored) {
+        }
+
+
+
         if (isPlaying()) {
             stopPlay();
         }
 
         try {
+            proxy = new StreamProxy();
+            proxy.init();
+            proxy.start();
+
+            url = String.format("http://127.0.0.1:%d/%s", proxy.getPort(), url);
+
             playing = true;
             mediaPlayer = new MediaPlayer();
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -34,5 +51,6 @@ public class AllelonMediaPlayer {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
+        proxy.stop();
     }
 }

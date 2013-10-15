5c092dff7a3f1cd45c266d6eff88688bb231cc68
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2013-10-15 22:42:07 +0200

    Clear the notification if our process goes down killed by Android.

--- Allelon/src/main/java/com/ciplogic/allelon/service/MediaPlayerIntent.java
+++ Allelon/src/main/java/com/ciplogic/allelon/service/MediaPlayerIntent.java
@@ -3,6 +3,8 @@ package com.ciplogic.allelon.service;
 import android.app.IntentService;
 import android.content.Intent;
 
+import com.ciplogic.allelon.notification.StreamNotification;
+
 public class MediaPlayerIntent extends IntentService {
     private ThreadMediaPlayer allelonMediaPlayer = ThreadMediaPlayer.getInstance(this);
 
@@ -13,5 +15,13 @@ public class MediaPlayerIntent extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         allelonMediaPlayer.run();
+    }
+
+    @Override
+    public void onDestroy() {
+        // make sure the notification also goes down, if Android nukes our process.
+        new StreamNotification(this).hideNotification();
+
+        super.onDestroy();
     }
 }

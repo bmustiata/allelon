2451a2e03f43b09866c0086da8e3b5a554b31d14
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2013-12-26 01:10:36 +0100

    Some progress towards having tabs.

---
+++ Allelon/src/main/java/com/ciplogic/allelon/AllelonActivity.java
@@ -0,0 +1,46 @@
+package com.ciplogic.allelon;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+/**
+ * Global application that picks the tabs.
+ */
+public class AllelonActivity extends TabActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.allelon_activity);
+        setTabs() ;
+    }
+    private void setTabs()
+    {
+        addTab("Home", R.drawable.tab_home, RadioActivity.class);
+        addTab("Home", R.drawable.tab_home, RadioActivity.class);
+        addTab("Home", R.drawable.tab_home, RadioActivity.class);
+    }
+
+    private void addTab(String labelId, int drawableId, Class<?> c)
+    {
+        TabHost tabHost = getTabHost();
+        Intent intent = new Intent(this, c);
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);
+
+        View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
+        TextView title = (TextView) tabIndicator.findViewById(R.id.title);
+        title.setText(labelId);
+        ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
+        icon.setImageResource(drawableId);
+
+        spec.setIndicator(tabIndicator);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+    }
+
+}

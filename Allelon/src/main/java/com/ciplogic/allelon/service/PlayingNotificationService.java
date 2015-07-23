6c7ac71e683fb4e0a4f148c1aede6fe9d459a4d9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-07-23 22:27:51 +0200

    Notifications also show the title correctly now.

--- Allelon/src/main/java/com/ciplogic/allelon/service/PlayingNotificationService.java
+++ Allelon/src/main/java/com/ciplogic/allelon/service/PlayingNotificationService.java
@@ -6,9 +6,10 @@ import com.ciplogic.allelon.eventbus.Event;
 import com.ciplogic.allelon.eventbus.EventBus;
 import com.ciplogic.allelon.eventbus.EventListener;
 import com.ciplogic.allelon.eventbus.events.MediaPlayerComponentStatusEvent;
+import com.ciplogic.allelon.eventbus.events.MediaPlayerTitleEvent;
 import com.ciplogic.allelon.notification.StreamNotification;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,26 +19,53 @@ import java.util.List;
  */
 public class PlayingNotificationService implements EventListener {
     private final StreamNotification streamNotification;
+    private boolean playing = false;
 
     public PlayingNotificationService(Context context) {
         this.streamNotification = new StreamNotification(context);
         EventBus.INSTANCE.registerListener(this);
     }
 
+    /**
+     * This will check for PREPARING/DESTROYING player component events, since they are
+     * the most reliable in reporting the status of the player, and the MediaPlayerTitleEvent.
+     *
+     * This will update the status of the played song, only if the player is actually playing,
+     * in order to avoid out of band title events that would just show the playing notification
+     * when the player is not actually playing.
+     *
+     * @param event
+     */
     @Override
     public void onEvent(Event event) {
         if (event instanceof MediaPlayerComponentStatusEvent) {
             MediaPlayerComponentStatusEvent mediaPlayerComponentStatus = (MediaPlayerComponentStatusEvent)event;
             if (mediaPlayerComponentStatus.playerState == PlayerState.PREPARING) {
                 streamNotification.showNotification("Playing...");
+                playing = true;
             } else if (mediaPlayerComponentStatus.playerState == PlayerState.DESTROYING) {
                 streamNotification.hideNotification();
+                playing = false;
             }
+        }
+
+        if (event instanceof MediaPlayerTitleEvent) {
+            if (!playing) {
+                return;
+            }
+
+            MediaPlayerTitleEvent mediaPlayerTitleEvent = (MediaPlayerTitleEvent)event;
+            streamNotification.showNotification(mediaPlayerTitleEvent.title);
         }
     }
 
     @Override
     public List<Class<? extends Event>> getListenedEvents() {
-        return (List) Collections.singletonList(MediaPlayerComponentStatusEvent.class);
+        List result = new ArrayList();
+
+        result.add(MediaPlayerTitleEvent.class);
+        result.add(MediaPlayerComponentStatusEvent.class);
+
+        return result;
     }
 }

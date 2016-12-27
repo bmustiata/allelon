659d3b01f81d72ec0feb9a98525ec8a370361b14
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-12-27 09:40:14 +0100

    Fixed user permissions.

--- bin/build-script.sh
+++ bin/build-script.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+echo "Running as $UID:$GID"
+
+#
+# Run the actual build.
+#
 cd /project
 gradle build
+
+#
+# fix permissions
+#
+chown -R $UID:$GID build Allelon/build
 
--- bin/run-build.sh
+++ bin/run-build.sh
@@ -4,6 +4,8 @@ FOLDER=$(readlink -f $(dirname $(readlink -f "$0"))/..)
 
 docker run -it \
     --rm \
+    -e UID=$(id -u) \
+    -e GID=$(id -g) \
     -v $FOLDER:/project \
     bmst/android-build
 

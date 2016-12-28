8f222d086aa7eb71aaa7834a8ca89379bfe39a80
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-12-28 10:27:18 +0100

    The build including the signing works correctly now.

--- bin/build-script.sh
+++ bin/build-script.sh
@@ -9,7 +9,23 @@ cd /project
 gradle build
 
 #
-# fix permissions
+# If make release is active, run the signing of the binary.
 #
-chown -R $UID:$GID build Allelon/build
+if [[ $MAKE_RELEASE != "" ]]; then
+    echo "MAKE RELEASE"
+
+    cd /project/Allelon/build/outputs/apk
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /project/keystore.jks Allelon-release-unsigned.apk "allelon radio"
+
+    rm Allelon-release-aligned.apk
+
+    /android/build-tools/25.0.2/zipalign 4 Allelon-release-unsigned.apk Allelon-release-aligned.apk
+
+    echo Final binary is: Allelon-release-aligned.apk
+fi # [[ $MAKE_RELEASE != "" ]]
+
+#
+# Fix permissions.
+#
+chown -R $UID:$GID /project/build /project/Allelon/build
 
--- bin/run-build.sh
+++ bin/run-build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 FOLDER=$(readlink -f $(dirname $(readlink -f "$0"))/..)
+#MAKE_RELEASE=
+
+if [[ "$1" == "-r" ]]; then
+    MAKE_RELEASE=1
+fi # [[ "$1" == "-r" ]]
 
 docker run -it \
     --rm \
     -e UID=$(id -u) \
     -e GID=$(id -g) \
+    -e MAKE_RELEASE=$MAKE_RELEASE \
     -v $FOLDER:/project \
     bmst/android-build
 
